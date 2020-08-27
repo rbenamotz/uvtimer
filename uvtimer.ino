a334a9656990b91197745c0ac404add6b9ebdf27
Roey Benamotz <roey@benamotz.com>
2020-08-27 11:23:03 -0400

    Initial commit

---
+++ uvtimer.ino
@@ -0,0 +1,46 @@
+#include <Encoder.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 4);
+
+
+Encoder myEnc(2, 3);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("UV Timer");
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.display();
+  display.clearDisplay();
+}
+
+long oldPosition  = -999;
+
+void loop() {
+  long newPosition = myEnc.read() / 4;
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+    updateDisplay();
+  }
+}
+
+void updateDisplay() {
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.print(oldPosition);
+  display.display();
+}
